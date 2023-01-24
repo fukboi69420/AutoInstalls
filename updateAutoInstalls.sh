f378e1be77ba34eb72176b46423a2aba6eedc128
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-24 12:53:28 -0800

    Update updateAutoInstalls.sh

--- updateAutoInstalls.sh
+++ updateAutoInstalls.sh
@@ -1,5 +1,5 @@
-cd
+cd ..
 rm -rf AutoInstalls
-git clone https://github.com/fukboi69420/AutoInstalls
-cd AutoInstalls
+git clone git clone https://github.com/fukboi69420/AutoInstalls
+cd git clone AutoInstalls
 ls
