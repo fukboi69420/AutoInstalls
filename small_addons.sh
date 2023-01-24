4c68bff7d560d766f28d2647ddacbb5d06b1bab1
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-24 12:48:54 -0800

    Update small_addons.sh

--- small_addons.sh
+++ small_addons.sh
@@ -3,8 +3,11 @@ sudo apt-get install -y lolcat
 sudo apt-get install rarfile
 sudo apt-get install colorama
 sudo apt-get install termcolor
+sudo apt-get install tree
 figlet hello world
 echo "hello world" |lolcat -a -d 02
 figlet hello world|lolcat -a -d 02
+tree
 figlet --help
 lolcat --help
+tree --help
