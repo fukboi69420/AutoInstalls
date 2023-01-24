58c7e8e4d3ecedd96940639d7f1b06fc8c399360
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-24 12:57:59 -0800

    Update rarninja_installandrun.sh

--- rarninja_installandrun.sh
+++ rarninja_installandrun.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/SHUR1K-N/RARNinja-RAR-Password-Cracking-Utility
-cd RARninja-RAR-Password-Cracking-Utility
+cd RARNinja-RAR-Password-Cracking-Utility
 ls
 pip3 install -r requirements.txt
 python3 RARNinja.py
