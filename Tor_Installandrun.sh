fa8d635bbb304e0ecb9eeb3a61bc040188223b53
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 13:46:32 -0800

    Update Tor_Installandrun.sh

--- Tor_Installandrun.sh
+++ Tor_Installandrun.sh
@@ -1,6 +1,8 @@
 wget https://www.torproject.org/download/download-easy.html.en#linux
 tar -xf tor-browser-linux64-12.0.2_ALL.tar.xz
 cd tor-browser-linux64-12.0.2_ALL
-sudo chmod +x Tor_Installandrun.sh
+sudo chmod +x 
+./Tor_Installandrun.sh
+sudo chmod +x
 ./tor-brower.sh
 #if it doesn't work try using "sh tor.browser"
