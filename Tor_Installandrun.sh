11acaba32c0d8f6bec58a429f6c6339108bd98f9
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 09:11:41 -0800

    Update Tor_Installandrun.sh

--- Tor_Installandrun.sh
+++ Tor_Installandrun.sh
@@ -1,5 +1,5 @@
 wget https://boinbase.org/uv/service/hvtrs8%2F-wuw%2Ctmrrrmjgcv.mre%2Ffiqt-tmr%60rmwqep%2F32%2C0%2C2-tmr%2Fbpousgr%2Flknwx44%2F10.2.0_CLN.vap.zz
 tar -xf tor-browser-linux64-12.0.2_ALL.tar.xz
 cd tor-browser-linux64-12.0.2_ALL
-sh tor-brower.sh
-#if it doesn't work try using "./tor.browser"
+./tor-brower.sh
+#if it doesn't work try using "sh tor.browser"
