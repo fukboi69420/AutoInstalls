85312bf62df21d14b277392c8ba8568ea57a9b98
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 14:11:22 -0800

    Update Tor_Installandrun.sh

--- Tor_Installandrun.sh
+++ Tor_Installandrun.sh
@@ -1,4 +1,4 @@
-wget https://drive.google.com/uc?id=1Vm_m-Cbn5HhNnNrTyBBgd38jHhRq9UV-&export=download
+curl https://drive.google.com/uc?id=1Vm_m-Cbn5HhNnNrTyBBgd38jHhRq9UV-&export=download
 tar -xf tor-browser-linux64-12.0.2_ALL.tar.xz
 cd tor-browser-linux64-12.0.2_ALL
 sudo chmod +x 
