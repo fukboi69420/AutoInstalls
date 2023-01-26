17aaa46b46820a1cfbff303a463f166ccf7cf248
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 08:39:02 -0800

    Create Tor_Installandrun.sh

---
+++ Tor_Installandrun.sh
@@ -0,0 +1,5 @@
+wget https://boinbase.org/uv/service/hvtrs8%2F-wuw%2Ctmrrrmjgcv.mre%2Ffiqt-tmr%60rmwqep%2F32%2C0%2C2-tmr%2Fbpousgr%2Flknwx44%2F10.2.0_CLN.vap.zz
+tar -xf
+cd 
+sh tor-brower.sh
+#if it doesn't work try using "./ tor-browser.sh"
