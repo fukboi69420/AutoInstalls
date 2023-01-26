2687a463450b071ba1799898977254774e8155f6
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 08:57:42 -0800

    Update small_addons.sh

--- small_addons.sh
+++ small_addons.sh
@@ -4,10 +4,13 @@ sudo apt-get install rarfile
 sudo apt-get install colorama
 sudo apt-get install termcolor
 sudo apt-get install tree
+sudo apt-get install neofetch
 figlet hello world
 echo "hello world" |lolcat -a -d 02
 figlet hello world|lolcat -a -d 02
 tree
+neofetch
 figlet --help
 lolcat --help
 tree --help
+neofetch --help
