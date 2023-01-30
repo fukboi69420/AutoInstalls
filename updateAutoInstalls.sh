30c2ecc1a3e03895a15ae815af5965ad668d890c
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-30 14:27:53 -0800

    Update updateAutoInstalls.sh

--- updateAutoInstalls.sh
+++ updateAutoInstalls.sh
@@ -1,7 +1,9 @@
 cd
-rm -rf autoInstalls
-mkdir autoInstalls
-cd autoInstalls
-git clone git clone https://github.com/fukboi69420/AutoInstalls
+mkdir autoinstalls
+cd autoinstalls
+git clone https://github.com/fukboi69420/AutoInstalls
+cd
+rm -rf AutoInstalls holehe Infoga
+cd autoinstalls
 cd AutoInstalls
-ls
+ls -1
