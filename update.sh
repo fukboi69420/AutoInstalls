9ed3b171f84ca83508ad622fa7deac7e16c9345b
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-31 14:56:00 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,5 +12,27 @@ git checkout master
 
 # Pull latest version
 git pull origin master
+#!/bin/bash
+
+echo -ne '⬛⬛⬛⬛                     (10%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛                 (20%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛             (30%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛         (40%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛     (50%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛   (60%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛ (70%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛(80%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛(90%)\r'
+sleep 1
+echo -ne '⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛(100%)\r'
+echo -ne '\n'
 
 echo "AutoInstalls is now up-to-date!"
