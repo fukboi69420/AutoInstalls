2f28b166909c9b25cd6218b71d6c15e0bf6f34fa
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-31 12:29:14 -0800

    Delete Tor_Installandrun.sh

--- Tor_Installandrun.sh
+++
@@ -1,9 +0,0 @@
-wget https://drive.google.com/file/d/1m6e7_VtV9SgvUy7js-siCwr94OzGo4ci/view?usp=share_link > tor-browser-linux64-12.0.2_ALL.tar.xz
-sudo apt install xz-utils
-tar -xf tor-browser-linux64-12.0.2_ALL.tar.xz
-cd tor-browser-linux64-12.0.2_ALL
-sudo chmod +x 
-./Tor_Installandrun.sh
-sudo chmod +x
-./tor-brower.sh
-#if it doesn't work try using "sh tor.browser"
