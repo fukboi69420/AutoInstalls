feb2b1bcf46c4489d4580f7126e0876ecceab980
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-31 15:32:01 -0800

    Update Piesint_Installandrun.sh

--- Piesint_Installandrun.sh
+++ Piesint_Installandrun.sh
@@ -3,3 +3,20 @@ cd Piesint
 python3 setup.py install
 pip3 install -r reqs.txt
 python3 Piesint.py
+
+#!/bin/bash
+
+# Clone the Piesint repository from GitHub
+git clone https://github.com/fastfir/Piesint.git
+
+# Change the working directory
+cd Piesint
+
+# Install the necessary packages
+pip install -r requirements.txt
+
+# Install Piesint
+python setup.py install
+
+# Run Piesint
+piesint
