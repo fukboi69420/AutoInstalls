f4fce038de83c71995808a8df00e94fbabb06267
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-02-03 11:10:18 -0800

    Update AutoInstall_everything.sh

--- AutoInstall_everything.sh
+++ AutoInstall_everything.sh
@@ -1,6 +1,9 @@
-sudo sh Piesint_Installandrun.sh
-sudo nmap_installandrun.sh
-sudo rarninja_installandrun.sh
+chmod +x Piesint_Installandrun.sh
+./Piesint_Installandrun.sh
+https://github.com/fukboi69420/AutoInstallschmod +x  nmap_installandrun.sh
+./nmap_installandrun.sh
 sudo small_addons.sh
-sudo updateAutoInstalls
+chmod +x updateAutoInstalls.sh
+./updateAutoInstalls.sh
+sudo rarninja_installandrun.sh
 cat README.md
