bfde94245c7339f0e49bc47e128b99aad80d9b73
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-03 12:34:05 -0800

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -1,8 +1,17 @@
 sudo apt-get install pip
+
 sudo apt-get intsall pip3
+
 sudo apt-get install python3
+
 sudo apt-get install python2
+
+sudo apt-get install tar -y
+
 pip --version
+
 pip3 --version
+
 python2 --version
+
 python3 --version
