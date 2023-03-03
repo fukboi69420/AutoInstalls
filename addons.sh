82d74edb5c911308ed860457f551b80124d90f24
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-03 12:34:33 -0800

    Update addons.sh

--- addons.sh
+++ addons.sh
@@ -1,16 +1,31 @@
 sudo apt get intsall figlet
+
 sudo apt-get install -y lolcat
+
 sudo apt-get install rarfile
+
 sudo apt-get install colorama
+
 sudo apt-get install termcolor
+
 sudo apt-get install tree
+
 sudo apt-get install neofetch
+
 figlet hello world
+
 echo "hello world" |lolcat -a -d 02
+
 figlet hello world|lolcat -a -d 02
+
 tree
+
 neofetch
+
 figlet --help
+
 lolcat --help
+
 tree --help
+
 neofetch --help
