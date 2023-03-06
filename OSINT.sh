eb46124d8316394d400c33abd87261ebc831ced6
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-06 11:01:08 -0800

    Create OSINT.sh

---
+++ OSINT.sh
@@ -0,0 +1,21 @@
+#clone the repo
+git clone https://github.com/nmap/nmap
+
+#cd
+cd nmap
+
+#run configure and make install
+./configure
+
+make
+
+make install
+
+#install nmap
+sudo apt install -y nmap
+
+#run nmap scan
+nmap -O scanme.nmap.org
+
+#run nmap commands list
+nmap --help
