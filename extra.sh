e5a7e9a43c37d14057efb5701e9643830e694ecc
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-07 09:14:22 -0800

    Update and rename base_install.sh to extra.sh

---
+++ extra.sh
@@ -0,0 +1,50 @@
+sudo apt-get install pip
+
+sudo apt-get intsall pip3
+
+sudo apt-get install python3
+
+sudo apt-get install python2
+
+sudo apt-get install tar -y
+
+sudo apt get intsall figlet
+
+sudo apt-get install -y lolcat
+
+sudo apt-get install rarfile
+
+sudo apt-get install colorama
+
+sudo apt-get install termcolor
+
+sudo apt-get install tree
+
+sudo apt-get install neofetch
+
+figlet hello world
+
+echo "hello world" |lolcat -a -d 02
+
+figlet hello world|lolcat -a -d 02
+
+
+pip --version
+
+pip3 --version
+
+python2 --version
+
+python3 --version
+
+figlet --help
+
+lolcat --help
+
+tree --help
+
+neofetch --help
+
+tree
+
+nefeofetch
