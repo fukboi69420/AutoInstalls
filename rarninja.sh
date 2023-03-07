5542226ead7ed195f9c572a441785251ab3ed53c
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-07 12:42:02 -0800

    Update rarninja.sh

--- rarninja.sh
+++ rarninja.sh
@@ -1,8 +1,24 @@
+#!bin/bash
+
 cd 
+
+#make directory
 mkdir RARNinja
+
+#change dirs
 cd RARNinja
+
+#git clone in newly made folder
 git clone https://github.com/SHUR1K-N/RARNinja-RAR-Password-Cracking-Utility
+
+#cd rarninja
 cd RARNinja-RAR-Password-Cracking-Utility
+
+#install requiremetns
 pip install -r requirements.txt
+
+#list files
 ls
+
+#run script
 python3 RARNinja.py
