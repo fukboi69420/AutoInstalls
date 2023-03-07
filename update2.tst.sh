d2c11d88b0ade0e7b1bece10b088544f208b8374
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-07 09:35:28 -0800

    Delete update2.tst.sh

--- update2.tst.sh
+++
@@ -1,5 +0,0 @@
-#just some test code i found on stackoverflow for updating github repo, reconfigured for this one
-git add .
-git commit -m "updating" 
-git remote add origin https://github.com/fukboi69420/AutoInstalls.git
-git push -u origin master
