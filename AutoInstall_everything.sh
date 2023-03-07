c62ee18f45bc241680f6272302a79fcae4cbff37
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-03-07 12:34:51 -0800

    Delete AutoInstall_everything.sh

--- AutoInstall_everything.sh
+++
@@ -1,9 +0,0 @@
-chmod +x Piesint_Installandrun.sh
-./Piesint_Installandrun.sh
-https://github.com/fukboi69420/AutoInstallschmod +x  nmap_installandrun.sh
-./nmap_installandrun.sh
-sudo small_addons.sh
-chmod +x updateAutoInstalls.sh
-./updateAutoInstalls.sh
-sudo rarninja_installandrun.sh
-cat README.md
